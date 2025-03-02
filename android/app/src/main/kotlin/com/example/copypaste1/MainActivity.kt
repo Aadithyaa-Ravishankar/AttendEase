f278e364d60405c11412816a73f35a530689a31c
Aadithyaa-Ravishankar <164360061+Aadithyaa-Ravishankar@users.noreply.github.com>
2025-03-02 12:17:15 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/copypaste1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.copypaste1
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/copypaste1/MyApplication.kt
@@ -0,0 +1,10 @@
+package com.example.copypaste1
+
+import android.app.Application  // Correct import for Application class
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Initialize anything if needed
+    }
+}
